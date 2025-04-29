058b6c4d31006d170bfb0ad87160177769c42aec
lemanrza <lemaner@code.edu.az>
2025-04-29 17:22:10 +0400

    feat: email and password checked

--- src/UserLogin/UserLogin.jsx
+++ src/UserLogin/UserLogin.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserLogin = () => {
-    function login(e) {
-        e.preventDefault()
-        console.log("E-mail üçün: 'Yalnız .ru domenlərinə icazə verilir.' \n Parol üçün: 'Ən azı 8 simvol'.")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
     }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function login(e) {
+        e.preventDefault();
+        if (!email.endsWith(".ru")) {
+            console.log("Yalnız .ru domenlərinə icazə verilir.");
+            return;
+        }
+        if (password.length < 8) {
+            console.log("Parol ən azı 8 simvol olmalıdır.");
+            return;
+        }
+        console.log("Logged in successfully!");
+    }
+
     return (
         <div className="h-[90vh] flex items-center justify-center">
             <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm">
@@ -12,13 +31,15 @@ const UserLogin = () => {
                 <form className="flex flex-col gap-4">
                     <input
                         type="email"
+                        onChange={handleEmailChange}
                         placeholder="Email"
                         className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none"
                     />
                     <input
                         type="password"
+                        onChange={handlePasswordChange}
                         placeholder="Password"
-                        className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2"
                     />
                     <button
                         type="submit" onClick={login}
